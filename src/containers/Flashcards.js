afaecb59ad25cc3b0868783383d3aaf6ffbf160c
Ben Denzer <denzer.ben@gmail.com>
2016-10-15 03:26:55 -0700

    success screen showing/hiding

--- src/containers/Flashcards.js
+++ src/containers/Flashcards.js
@@ -2,29 +2,40 @@ import React, {Component} from 'react';
 import ScoreBox from '../components/ScoreBox';
 import Card from '../components/Card';
 import {recognition} from '../logic/speachConfig';
+import {Spinner} from '../components/Spinner';
 
 class Flashcards extends Component {
     constructor(props) {
         super(props);
+        this.state = {showCoin: false, update: true, coinHasBeenShown: false};
         this.showCoin = this.showCoin.bind(this);
+        //this.showWord = this.showWord.bind(this);
+    }
+    componentWillReceiveProps() {
+        if (this.state.coinHasBeenShown) this.setState({coinHasBeenShown: false});
+    }
+    shouldComponentUpdate() {
+        return this.state.update ? true : false;
     }
     componentDidUpdate() {
-        if (this.props.score % 3 === 0 && this.props.score) {
+        if (this.props.score % 3 === 0 && this.props.score && !this.state.coinHasBeenShown) {
             this.showCoin();
+            this.setState({update: false});
         }
     }
     componentWillUnmount() {
         recognition.stop();
     }
     showCoin() {
-        console.log('showcoin called');
+        this.setState({showCoin: true});
+        setTimeout(() => this.setState({showCoin: false, update: true, coinHasBeenShown: true}), 4000);
     }
     render() {
+        console.log(this.state.coinHasBeenShown);
         return (
             <div id="container">
                 <ScoreBox name={this.props.name} score={this.props.coins} />
-                <Card word={this.props.word} />
-                <audio ref={(ref) => this.myDing = ref} id="myDing" src="../../ding.mp3" />
+                <Card word={this.state.showCoin ? <Spinner /> : this.props.word} />
             </div>
         );
     }
